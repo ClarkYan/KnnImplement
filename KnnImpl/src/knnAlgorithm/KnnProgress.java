788a6fe031bd844ed420d9e5f7aba946c4b203db
Clark YAN <clarkyan1993@gmail.com>
2016-11-24 23:22:59 +0800

    update project

--- KnnImpl/src/knnAlgorithm/KnnProgress.java
+++ KnnImpl/src/knnAlgorithm/KnnProgress.java
@@ -8,7 +8,12 @@ public class KnnProgress {
 	//calculate the distance between two elements in the list
 	public double calDistance (List<Double> d1, List<Double> d2)
 	{
+		int i = 0;
 		double distance = 0;
+		for(i=0; i<d1.size(); i++)
+		{
+			distance += (d1.get(i) - d2.get(i)) * (d1.get(i) - d2.get(i));
+		}
 		return distance;
 	}
 }
